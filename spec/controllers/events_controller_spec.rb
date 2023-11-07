c41b5f7013f0378f4d8d2ef16a70617a76ff350e
System Administrator <root@Akshays-MacBook-Air.local>
2023-11-07 12:07:09 +0530

    simple cov and test cases added

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -23,5 +23,37 @@ describe EventsController, type: :controller do
 	response.status == 302
     expect(flash[:notice]).to eq("Event B created successfully!")
   end
+
+   it "should create Event A" do
+    stub_request(:post, "https://api.iterable.com/api/docs/events/track").
+         with(
+           body: "{\"email\":\"#{@user.email}\",\"eventName\":\"Event A\",\"dataFields\":{}}",
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Api-Key'=>'hbjhvdjcusjvcjasv',
+          'User-Agent'=>'Ruby'
+           }).
+         to_return(status: 200, body: "", headers: {})
+    post :create_event_a
+  response.status == 302
+    expect(flash[:notice]).to eq("Event A created successfully!")
+  end
+
+   it "Send Email Event B" do
+    stub_request(:post, "https://api.iterable.com/api/docs/email/target").
+         with(
+           body: "{\"email\":\"#{@user.email}\",\"eventName\":\"Event A\",\"dataFields\":{}}",
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Api-Key'=>'hbjhvdjcusjvcjasv',
+          'User-Agent'=>'Ruby'
+           }).
+         to_return(status: 200, body: "", headers: {})    
+    post :send_email_for_b
+  response.status == 302
+    expect(flash[:notice]).to eq("Event B Email sent successfully!")
+  end   
 end
 
